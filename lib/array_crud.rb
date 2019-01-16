fd0ad236c9e4d1c51cc7fbd0bf00a76c8fe1caba
Charlotte Pearce <charlotte.pearce1984@gmail.com>
2019-01-16 20:05:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  colours = ["blue", "white", "green"]
+  words << "yellow"
 end
 
 def add_element_to_start_of_array(array, element)
